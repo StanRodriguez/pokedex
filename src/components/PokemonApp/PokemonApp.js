bdd153eb91cf1150106d523aa92490245907b2a6
Stanly Rodriguez <ibestanly@gmail.com>
2019-05-14 12:54:01 -0400

    Change boostrap position

--- src/components/PokemonApp/PokemonApp.js
+++ src/components/PokemonApp/PokemonApp.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import Header from "../Header/Header";
 import Search from "../Search/Search";
 import PokemonList from "../PokemonList/PokemonList";
