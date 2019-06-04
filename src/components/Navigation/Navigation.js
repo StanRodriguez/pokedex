f055a0439fb445344e535be7fd23cc447e45de2c
Stanly Rodriguez <ibestanly@gmail.com>
2019-06-03 20:14:16 -0400

    Clean up a little bit

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -13,18 +13,12 @@ export default function Pagination({
   function handleNext() {
     const range = [];
     if (last + limit > pokemonsTotal) {
-      for (let i = last + 1; i <= pokemonsTotal; i++) {
-        range.push(i);
-      }
+      for (let i = last + 1; i <= pokemonsTotal; i++) range.push(i);
+
       const length = range.length;
-      for (let i = 1; i <= limit - length; i++) {
-        range.push(i);
-      }
-    } else {
-      for (let i = last + 1; i <= last + limit; i++) {
-        range.push(i);
-      }
-    }
+
+      for (let i = 1; i <= limit - length; i++) range.push(i);
+    } else for (let i = last + 1; i <= last + limit; i++) range.push(i);
 
     getPokemons(range);
   }
